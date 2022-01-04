e7af7cfe7a2ef262ec018dc4f2a750a69d00a237
Markus Kowalewski <markus.kowalewski@gmail.com>
2022-01-04 22:23:09 +0100

    fix nix-shell

--- nix/module.nix
+++ nix/module.nix
@@ -98,7 +98,7 @@ in {
 
       nixShell = job: pkgs.writeScript "nixShell" ''
         #!/usr/bin/env nix-shell
-        #nix-shell -i bash -p ${job.env}
+        #!nix-shell -i bash -p ${job.env}
 
         cd ${job.workdir}
         ${job.jobScript}
